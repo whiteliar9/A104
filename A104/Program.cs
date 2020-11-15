805de8abdf29d33b30e9ce674bc5a99d4c041b3c
whitelior <whiteliar0505@gmail.com>
2020-11-15 10:31:22 +0900

    초기 커밋.

---
+++ A104/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace A104
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lstNames = new List<string>();
+            lstNames.Add("dogs");
+            lstNames.Add("cow");
+            lstNames.Add("rabbit");
+            lstNames.Add("goat");
+            lstNames.Sort();
+            foreach (string s in lstNames)
+                Console.Write(s + " ");
+            Console.WriteLine();
+
+            string[] arrNames = new string[100];
+            arrNames[0] = "dog";
+            arrNames[1] = "cow";
+            arrNames[2] = "rabbit";
+            arrNames[3] = "goat";
+            arrNames[1] = "sheep";
+            Array.Sort(arrNames);
+            foreach (string s in lstNames)
+                Console.Write(s + " ");
+            Console.WriteLine();
+         
+        }
+    }
+}
